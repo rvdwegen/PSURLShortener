4929f9c5168d911ab3e28c24b297fec1ed1b9531
Roel van der Wegen <github@aeternus.tech>
2024-02-15 22:26:56 +0100

    .

--- profile.ps1
+++ profile.ps1
@@ -8,7 +8,7 @@
 #
 # You can define helper functions, run commands, or specify environment variables
 # NOTE: any variables defined that are not environment variables will get reset after the first execution
-
+ 
 # Import modules
 @('Az.Accounts','AzBobbyTables') | ForEach-Object {
     try {
