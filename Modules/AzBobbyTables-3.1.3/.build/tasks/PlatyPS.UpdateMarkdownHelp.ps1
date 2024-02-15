51d76eea258214175dcacdf9c4125ff6a63e238b
Roel van der Wegen <github@aeternus.tech>
2024-02-15 23:25:15 +0100

    .

--- Modules/AzBobbyTables-3.1.3/.build/tasks/PlatyPS.UpdateMarkdownHelp.ps1
+++
@@ -1,6 +0,0 @@
-task updateMarkdownHelp {
-    Import-Module 'platyPS' -ErrorAction 'Stop'
-    $ProjectName = Get-SamplerProjectName -BuildRoot $BuildRoot
-    Import-Module "$BuildModuleOutput/$ProjectName" -Force -ErrorAction 'Stop'
-    Update-MarkdownHelpModule -Path "$BuildRoot/$HelpSourceFolder/$HelpOutputFolder"
-}
