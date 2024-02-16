b7786271c642e8008e34a12513775dc1faa0f03e
Roel van der Wegen <github@aeternus.tech>
2024-02-16 13:35:24 +0100

    .

--- short/run.ps1
+++ short/run.ps1
@@ -24,7 +24,7 @@ $obj = [pscustomobject]@{
     description = $Request.Query.Description
     slug = $slug
     counter = 0
-    hosts = $hosts
+    hosts = $env:WEBSITE_HOSTNAME
 }
 
 # write obj to table
