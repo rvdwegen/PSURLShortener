4d441040b7bc9afc8b5d93a4825ed6c4096d7a02
Roel van der Wegen <github@aeternus.tech>
2024-02-16 09:58:08 +0100

    .

--- short/run.ps1
+++ short/run.ps1
@@ -5,15 +5,19 @@ param($Request, $TriggerMetadata)
 
 $StatusCode = [HttpStatusCode]::OK
 
-$slug = (("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789").ToCharArray() | Get-Random -Count 8) -Join ""
+if ($Request.Query.slug) {
+    $slug = $Request.Query.slug
+} else {
+    $slug = (("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789").ToCharArray() | Get-Random -Count 8) -Join ""
+}
 
 $hosts = (Get-AzWebApp -Name vdwegen-urlshort).HostNames
 
 $obj = [pscustomobject]@{
-    originalURL = $Request.Query.
+    originalURL = $Request.Query.URL
     shortURL = "https://short.vdwegen.app/$slug"
-    description = ""
-    slug = if ($customSlug) { $customSlug } else { $slug }
+    description = $Request.Query.Description
+    slug = $slug
 }
 
 # write obj to table
