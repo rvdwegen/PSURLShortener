44d01f7c5147b9e03f2c3761de0e27cbadfa77d7
Roel van der Wegen <github@aeternus.tech>
2024-02-19 07:26:17 +0100

    move visit

--- Modules/helperFunctions.psm1
+++ Modules/helperFunctions.psm1
@@ -1,8 +1,6 @@
 function Invoke-URLRedirect {
     # Input bindings are passed in via param block.
     param($Request, $TriggerMetadata)
-    
-    $Request.Headers['x-ms-client-principal']
 
     try {
         Connect-AzAccount -Identity | Out-Null
@@ -15,27 +13,14 @@ function Invoke-URLRedirect {
         $urlObject = (Get-AzDataTableEntity -Filter "RowKey eq '$($Request.Params.URLslug)'" -context $urlTableContext)
 
         if ($urlObject) {
-            $visitsTableContext = New-AzDataTableContext -TableName 'visits' -StorageAccountName 'stourlshort' -ManagedIdentity
-            $visit = @{
-                PartitionKey = "VISIT"
-                RowKey = [string](New-Guid).Guid
-                ClientIp = $request.headers.'client-ip'
-                UserAgent = $request.headers.'user-agent'
-                Platform = $request.headers.'sec-ch-ua-platform'
-                slug = $urlObject.RowKey
-            }
-            Add-AzDataTableEntity -Entity $visit -context $visitsTableContext
-
-            # Increase visit count
-            $urlObject.visitors++
-            Update-AzDataTableEntity -Entity $urlObject -context $urlTableContext
-
             # Give a 302 response back
             $httpResponse = [HttpResponseContext]@{
                 StatusCode  = [HttpStatusCode]::Found
                 Headers     = @{ Location = $urlObject.originalURL }
                 Body        = ''
             }
+
+            $count = $true
         } else {
             # Get the notfound HTML content
             #$data = Get-Content -Path 'C:\home\site\wwwroot\Resources\notfound.html' -Raw
@@ -56,6 +41,23 @@ function Invoke-URLRedirect {
     Push-OutputBinding -Name Response -Value (
         $httpResponse
     )
+
+    if ($count) {
+        $visitsTableContext = New-AzDataTableContext -TableName 'visits' -StorageAccountName 'stourlshort' -ManagedIdentity
+        $visit = @{
+            PartitionKey = "VISIT"
+            RowKey = [string](New-Guid).Guid
+            ClientIp = $request.headers.'client-ip'
+            UserAgent = $request.headers.'user-agent'
+            Platform = $request.headers.'sec-ch-ua-platform'
+            slug = $urlObject.RowKey
+        }
+        Add-AzDataTableEntity -Entity $visit -context $visitsTableContext
+
+        # Increase visit count
+        $urlObject.visitors++
+        Update-AzDataTableEntity -Entity $urlObject -context $urlTableContext
+    }
 }
 
 Export-ModuleMember -Function @('Invoke-URLRedirect')
