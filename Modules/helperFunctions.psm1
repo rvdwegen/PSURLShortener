241bb85ac6f0bb208ad3df3548577b4c2a531e81
Roel van der Wegen <github@aeternus.tech>
2025-03-07 11:38:02 +0100

    ff

--- Modules/helperFunctions.psm1
+++ Modules/helperFunctions.psm1
@@ -4,6 +4,7 @@ function New-TableContext {
         [string]$TableName
     )
     $Context = New-AzDataTableContext -ConnectionString $env:AzureWebJobsStorage -TableName $TableName
+    New-AzDataTable -Context $Context | Out-Null
     return $Context
 }
 
