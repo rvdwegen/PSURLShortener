85fde4817c559158fdabad55922d0128d071aeaa
Roel van der Wegen <github@aeternus.tech>
2025-03-08 17:35:28 +0100

    dd

--- redirect/run.ps1
+++ redirect/run.ps1
@@ -3,18 +3,56 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
-$StatusCode = [HttpStatusCode]::OK
-
 $Slug = ([uri]$Request.Headers.'x-ms-original-url').Segments[1]
 
-$Request.Headers.'x-ms-original-url'
+$urlTableContext = New-TableContext -TableName 'shorturls'
 
-$Request.query.path
+try {
+    $urlObject = (Get-AzDataTableEntity -Filter "RowKey eq '$($Slug)'" -context $urlTableContext)
 
-$Request | fl
+    if ($urlObject) {
+        # Give a 302 response back
+        $httpResponse = [HttpResponseContext]@{
+            StatusCode  = [HttpStatusCode]::Found
+            Headers     = @{ Location = $urlObject.originalURL }
+            Body        = ''
+        }
+
+        $count = $true
+    } else {
+        # Get the notfound HTML content
+        $datapath = (Join-Path -Path (Resolve-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath "..\Resources")) -ChildPath "notfound2.html")
+        $data = Get-Content -Path $datapath -Raw
+        $data = $data.Replace('{slugVariable}',$($Request.Params.URLslug))
+
+        $httpResponse = [HttpResponseContext]@{
+            StatusCode  = [HttpStatusCode]::OK
+            Headers     = @{ 'content-type' = 'text/html' }
+            Body        = $data
+        }
+    }
+} catch {
+    throw "Error on line $($_.InvocationInfo.ScriptLineNumber): $($_.Exception.Message)"
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = $StatusCode
-    Body       = $Slug
-})
+Push-OutputBinding -Name Response -Value (
+    $httpResponse
+)
+
+Write-Host "after output"
+if ($count) {
+    $visitsTableContext = New-TableContext -TableName 'visits'
+    $visit = @{
+        PartitionKey = $urlObject.RowKey
+        RowKey = [string](New-Guid).Guid
+        ClientIp = $request.headers.'client-ip'
+        UserAgent = $request.headers.'user-agent'
+        Platform = $request.headers.'sec-ch-ua-platform'
+    }
+    Add-AzDataTableEntity -Entity $visit -context $visitsTableContext
+
+    # Increase visit count
+    $urlObject.visitors++
+    Update-AzDataTableEntity -Entity $urlObject -context $urlTableContext
+}
