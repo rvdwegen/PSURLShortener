0037e9c97de97e3cfbd9257ea1d58fad88f0f329
Roel van der Wegen <github@aeternus.tech>
2025-03-09 17:54:41 +0100

    dd

--- listurl/run.ps1
+++ listurl/run.ps1
@@ -16,5 +16,5 @@ try {
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $StatusCode
-    Body       = $result
+    Body       = [array]$result
 })
